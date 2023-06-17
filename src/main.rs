e9cfb6fe2c79f837fbec07b7d073590fc1fa9a2b
ThePeiLin <pei_hang@qq.com>
2023-06-17 20:29:08 +0800

    save:unifont

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,41 @@
-#![cfg_attr(not(debug_assertions),windows_subsystem="windows")]
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+mod setup;
 use eframe::egui;
 
-fn main() ->Result<(),eframe::Error> {
-    let opts=eframe::NativeOptions{
-        initial_window_size:Some(egui::vec2(320.0,240.0)),
+fn main() -> Result<(), eframe::Error> {
+    tracing_subscriber::fmt::init();
+    let opts = eframe::NativeOptions {
+        initial_window_size: Some(egui::vec2(800.0, 600.0)),
         ..Default::default()
     };
-    eframe::run_native("fakepaint",
-                       opts,
-                       Box::new(|_cc|Box::<FakePaint>::default()),
-                       )
+    eframe::run_native(
+        "fakepaint",
+        opts,
+        Box::new(|cc| Box::new(FakePaint::new(cc))),
+    )
 }
 
-struct FakePaint{
-    greeting:String,
+struct FakePaint {
+    greeting: String,
 }
 
-impl Default for FakePaint{
-    fn default()->Self{
-        Self{
-            greeting:"hello, world!".into(),
+impl FakePaint {
+    fn new(cc: &eframe::CreationContext<'_>) -> Self {
+        setup::custom_fonts(&cc.egui_ctx);
+        cc.egui_ctx.set_visuals(egui::Visuals::dark());
+        Self {
+            greeting: "hello, world!".into(),
         }
     }
 }
 
-impl eframe::App for FakePaint{
-    fn update(&mut self,
-              ctx:&egui::Context,
-              _frame:&mut eframe::Frame,){
-        egui::CentralPanel::default().show(ctx,|ui|{
+impl eframe::App for FakePaint {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
             ui.heading(self.greeting.clone());
             ui.label(self.greeting.clone());
         });
     }
 }
+
---
+++ src/setup.rs
@@ -0,0 +1,21 @@
+
+use eframe::egui;
+
+pub fn custom_fonts(ctx: &egui::Context) {
+    let mut fonts = egui::FontDefinitions::default();
+    fonts.font_data.insert(
+        "unifont".into(),
+        egui::FontData::from_static(include_bytes!("../assets/unifont-15.0.06.ttf")),
+    );
+    fonts
+        .families
+        .entry(egui::FontFamily::Monospace)
+        .or_default()
+        .insert(0, "unifont".into());
+    fonts
+        .families
+        .entry(egui::FontFamily::Proportional)
+        .or_default()
+        .insert(0, "unifont".to_owned());
+    ctx.set_fonts(fonts);
+}
